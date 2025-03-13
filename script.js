50d8d43d97dc6c1194b38e371240730fc834b8fe
DoMilovec <dmilovec@gmail.com>
2025-03-13 19:18:05 +0100

    Add a button to remove the book and comments

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Query selectors
 const container = document.querySelector('.container');
 const newBtn = document.querySelector('#new-book');
 const dialog = document.querySelector('dialog');
@@ -7,50 +8,66 @@ const selectName = document.querySelector('#name');
 const selectAuthor = document.querySelector('#author');
 const selectPages = document.querySelector('#pages');
 
+// Library array creation
 const myLibrary = [];
 
+// Book constructor and function for adding the book to library
 function Book(name, author, pages){
     this.name = name;
     this.author = author;
     this.pages = pages;
     this.id = crypto.randomUUID();
 }
-
 function addBookToLibrary(name, author, pages){
     let book = new Book(name, author, pages);
     myLibrary.push(book);
 }
 
-// addBookToLibrary('The Great Gatsby', 'Scott Fitzgerald', 246);
-// addBookToLibrary('Ulysses', 'James Joyce', 514);
-// addBookToLibrary('In Search of Lost Time', 'Marcel Proust', 652);
-// addBookToLibrary('One Hundred Years of Solitude', 'Gabriel Garcia', 523);
-// addBookToLibrary('The Catcher in the Rye', 'J.D. Salinger', 423);
-// addBookToLibrary('Nineteen Eighty Four', 'George Orwell', 254);
-
-for (let i=0 ; i<myLibrary.length ; i++){
-    const card =  document.createElement('div');
-    card.classList.add('card');
-    container.appendChild(card);
-    card.textContent = myLibrary[i].name + ' by ' + myLibrary[i].author + ', has ' + myLibrary[i].pages + ' pages.';
-}
-
+// Add new book, opens modal on click
 newBtn.addEventListener('click', (event) => {
     dialog.showModal();
 });
 
+// Confirm button to add the book to the library
 confirmBtn.addEventListener('click', (event) => {
     event.preventDefault();
-    const card =  document.createElement('div');
-    card.classList.add('card');
-    container.appendChild(card);
-    card.textContent = selectName.value + ' by ' + selectAuthor.value + ', has ' + selectPages.value + ' pages.';
+    addBookToLibrary(selectName.value, selectAuthor.value, selectPages.value);
+
+    container.textContent = ''; // clears container on each book that is added
+
+    // Loop to add and display books in the library
+    for (let i=0 ; i<myLibrary.length ; i++){
+        const card =  document.createElement('div');
+        card.classList.add('card');
+        container.appendChild(card);
+        card.textContent = myLibrary[i].name + ' by ' + myLibrary[i].author + ', has ' + myLibrary[i].pages + ' pages.';
+        
+        // saving the book ID in the loop
+        const bookId = myLibrary[i].id;
+
+        // delete button and logic to remove specific book
+        const delBtn = document.createElement('button');
+        delBtn.textContent = 'X';
+        container.appendChild(delBtn);
+        delBtn.addEventListener('click', (event) => {
+            card.remove();
+            delBtn.remove();
+    
+            // find and remove the book from myLibrary
+            const indexToRemove = myLibrary.findIndex(book => book.id === bookId);
+            if (indexToRemove !== -1) {
+                myLibrary.splice(indexToRemove, 1); // remove the correct book
+            }
+    });
+    }
     dialog.close();
     selectName.value = '';
     selectAuthor.value = '';
-    selectPages.value = '';
+    selectPages.value = ''; 
+    
 });
 
+// Cancel button in adding the book modal
 cancelBtn.addEventListener('click', (event) => {
     event.preventDefault();
     dialog.close();
